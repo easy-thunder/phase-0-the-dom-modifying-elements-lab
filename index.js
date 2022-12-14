c9ca6c6b1c94f77b4db0f9d5cef8ea69a951673c
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-13 17:40:07 -0700

    commit tree 4a1d652b4cff56104b792f80e7cc83bccbef625c

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here!
+
+const main = document.querySelector("#main");
+
+function removeMain(){
+    main.remove()
+}
+
+removeMain()
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Jacob Diehl is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
